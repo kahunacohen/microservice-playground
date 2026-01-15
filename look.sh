44bf17e6ccb9e466123ca565b15c36a5d6b365c9
Aaron Cohen <aaroncohendev@gmail.com>
2026-01-15 20:41:28 +0200

    add look.sh

--- look.sh
+++ look.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash 
  
 # uses tree and cat to print out essential file structure
-# and contents for use to give to AI.
+# and file contents for use to give to AI for context about
+# this repo.
 echo "file structure"
 tree -L 3.
 echo "docker-compose.yaml"
 cat docker-compose.yaml
+echo "compile-proto.sh:"
+cat compile-proto.sh
 echo "Dockerfiles:"
 find . -type f -name "Dockerfile" -print0 |
 while IFS= read -r -d '' file; do
@@ -14,10 +17,12 @@ while IFS= read -r -d '' file; do
   echo
 done
 echo "protos:"
-find . -type f -name "*.proto" -print0 |
+find api -type f -name '*.proto' -print0 |
 while IFS= read -r -d '' file; do
   echo "===== $file ====="
   cat "$file"
   echo
 done
+echo "gateway-service/cmd/server.go:"
+cat gateway-service/cmd/server.go
 
